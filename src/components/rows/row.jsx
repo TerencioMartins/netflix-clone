b8df31c5152cad7687322659ec6097d892207a69
Terencio Martins <zerofirty0@gmail.com>
2022-03-07 17:11:25 -0300

    Update

--- src/components/rows/row.jsx
+++ src/components/rows/row.jsx
@@ -2,11 +2,14 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Axios from "../../api/requests/axios";
 import "./row.css";
+import { AiOutlineRight } from "react-icons/ai";
+import { AiOutlineLeft } from "react-icons/ai";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [scrollX, setScrollX] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
@@ -29,15 +32,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
   //       .catch((error) => console.log(error));
   //   }
   // };
+  function handleLeft() {
+    // let x = scrollX + Math.round(window.innerWidth / 2);
+    let x = scrollX + 500
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  }
+
+  function handleRight() {
+    let x = scrollX - 500;
+    setScrollX(x);
+  }
 
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row__posters">
+      <div className="row__posters" style={{ marginLeft: scrollX }}>
         {movies.map((movie) => (
           <img
             key={movie.id}
-            // onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -45,8 +60,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
             alt={movie.title || movie.name}
           />
         ))}
+        <div
+          className={`row__left ${isLargeRow && "row__leftLarge"}`}
+          onClick={handleLeft}
+        >
+          <AiOutlineLeft />
+        </div>
+        <div
+          className={`row__right ${isLargeRow && "row__rightLarge"}`}
+          onClick={handleRight}
+        >
+          <AiOutlineRight />
+        </div>
       </div>
-      {/* {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />} */}
     </div>
   );
 }
