08646a8ee297ec058f4d1c11c1f91be259e9208b
Terencio Martins <zerofirty0@gmail.com>
2022-03-07 14:52:37 -0300

    Deploying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import Row from "./components/rows/row";
+import requests from "./api/requests/requests";
+import Banner from "./components/banner/banner";
+import Nav from "./components/nav/nav";
 
 function App() {
+  useEffect(() => {
+    let el = document.querySelector(".app");
+    el.classList.add("fade-in");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appbackground">
+      <div className="app">
+        <Nav />
+        <Banner />
+        <Row
+          title="Só na Netflix"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow={true}
+        />
+        <Row title="Em alta" fetchUrl={requests.fetchTrending} />
+        <Row
+          title="Ação e aventura empolgantes para TV"
+          fetchUrl={requests.fetchActionMovies}
+        />
+        <Row title="Comédia" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Romances" fetchUrl={requests.fetchRomanceMovies} />
+        <Row
+          title="Terror assustador para TV"
+          fetchUrl={requests.fetchHorrorMovies}
+        />
+        <Row title="Documentários" fetchUrl={requests.fetchDocumantaries} />
+        <Row title="Mais votados" fetchUrl={requests.fetchTopRated} />
+      </div>
     </div>
   );
 }
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import './nav.css'
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else handleShow(false)
+        });
+        return() => {
+            window.removeEventListener('scroll')
+        }
+    }, [])
+
+    function netflix(){
+        window.location.reload()
+    }
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+        <img
+            onClick={netflix}
+            className='nav__logo'
+            src='https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png'
+            alt='netflix logo'
+        />
+        <a className='nav__portfolio' href='https://terenciomartins.netlify.app/' target={'_blank'}> Portfólio </a>
+        <img
+            className='nav__avatar'
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+            alt='netflix logo'
+            />
+    </div>
+  )
+}
+
+export default Nav;
